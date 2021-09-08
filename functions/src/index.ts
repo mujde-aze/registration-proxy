95b4d3b35e00accc62d587143ce76e844c203b9f
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-09-08 13:31:14 +1000

    Remove mutating functions and limit to app verification and logging for this initial release

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import * as functions from "firebase-functions";
-import {TransferTokenGenerator} from "./services/TransferTokenGenerator";
-import * as dayjs from "dayjs";
+// import {TransferTokenGenerator} from "./services/TransferTokenGenerator";
+// import * as dayjs from "dayjs";
 import {CallableContext} from "firebase-functions/lib/common/providers/https";
-import {ContactService} from "./services/ContactService";
+// import {ContactService} from "./services/ContactService";
 import {CaptchaVerificationService} from "./services/CaptchaVerificationService";
 import {RegistrationRequestTransformer} from "./services/RegistrationRequestTransformer";
 import {RegistrationRequest} from "./models/RegistrationRequest";
@@ -11,13 +11,14 @@ export const registerContact = functions.region("australia-southeast1")
     .https.onCall(async (data: RequestData, context) => {
       await verifyCallingApp(context, data);
 
-      const transferTokenGenerator = initializeTransferTokenGenerator();
-      const contactService = new ContactService(functions.config().dt.baseurl, transferTokenGenerator.getTransferToken());
       const contact = RegistrationRequestTransformer.transformToContact(data.registrationRequest);
 
       functions.logger.info(`Received request to create contact: ${JSON.stringify(contact)}`);
-      const responseId = await contactService.createContact(contact, functions.config().dt.defaultassignment);
-      functions.logger.info(`Successfully created contact with ID: ${responseId}`);
+
+      /* const transferTokenGenerator = initializeTransferTokenGenerator();
+        const contactService = new ContactService(functions.config().dt.baseurl, transferTokenGenerator.getTransferToken());
+        const responseId = await contactService.createContact(contact, functions.config().dt.defaultassignment);
+           functions.logger.info(`Successfully created contact with ID: ${responseId}`);*/
 
       return "success";
     });
@@ -46,10 +47,10 @@ async function verifyCallingApp(context: CallableContext, data: RequestData) {
   }
 }
 
-function initializeTransferTokenGenerator(): TransferTokenGenerator {
+/* function initializeTransferTokenGenerator(): TransferTokenGenerator {
   return new TransferTokenGenerator(functions.config().dt.token,
       functions.config().dt.site1, functions.config().dt.site2, dayjs.utc().format("YYYY-MM-DDHH"));
-}
+}*/
 
 interface RequestData {
     captchaToken: string,
